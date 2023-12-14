bede4af57560901f291b60a358383d33b122b060
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2023-12-14 16:39:28 +0700

    landing page and medicine page done

--- app/Views/landingpage.php
+++ app/Views/landingpage.php
@@ -77,11 +77,49 @@
                     </div>
                 </div>
             </div>
-            <div class="bg-white h-40 shadow-lg rounded-lg aspect-square flex justify-center items-cemter">
+            <div class="bg-white h-40 shadow-lg rounded-lg aspect-square flex justify-center items-cemter mb-40">
                 <div>
                     <img src="/landing/fitnessdietary.png" alt="/landing/fitnessdietary.png" class="bg-cover bg-center p-9 -mt-2">                    
                     <div class="-mt-8 ml-7">
                         <h1 class="font-poppins font-semibold text-xs text-left">Fitness and Dietary</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="FOOTER bg-[#4200FF] text-white rounded-t-lg border-12 p-8 pb-0">
+            <div class="">
+                <h1 class="text-3xl font-bold text-center">We Care About You</h1>
+                <h2 class="text-xl text-center p-2">Find Any Medicine You Need!</h2>
+                <div class="flex text-sm">
+                    <div class="block p-8">
+                        <div class="h-8 w-2"><br/></div>
+                        <h1 class="font-bold text-md">Contact Us</h1>
+                        <p>ITB Ganesa, Labtek V</p>
+                        <p>Jl. Ganesa No. 10, Kecamatan Coblong,<br/> Kota Bandung, 40132</p>
+                    </div>
+                    <div class="p-8 pt-12">
+                        <ul class="gap-4">
+                            <li class="flex py-1 gap-2">
+                                <img alt="ig" src="/landing/instagram.png"/>
+                                <p>@tst.hospital</p>
+                            </li>
+                            <li class="flex py-1 gap-2">
+                                <img alt="wa" src="/landing/whatsapp.png"/>
+                                <p>+62 111 1111 1111</p>
+                            </li>
+                            <li class="flex py-1 gap-2">
+                                <img alt="email" src="/landing/email.png"/>
+                                <p>tst.hospital@gmail.com</p>
+                            </li>
+                            <li class="flex py-1 gap-2">
+                                <img alt="website" src="/landing/website.png"/>
+                                <p>tst.hospital.id</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="w-full h-48 pt-40 align-bottom">
+                        <p class="text-right align-text-bottom">© 2023 TST Hospital. All rights reserved.</p>
                     </div>
                 </div>
             </div>
---
+++ app/Views/medicine.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacy</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body class="font-poppins bg-[#F5F5F5] h-screen">
+    <header class="bg-[#F5F5F5]">
+        <nav class="flex justify-between items-center w-[92%] mx-auto mr-44">
+            <div>
+                <img src="/logo.svg" alt="/logo.svg">
+            </div>
+            <div class="h-full flex-col font-poppins font-semibold md:static absolute bg-[#F5F5F5] md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 ">
+                <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+                    <li>
+                        <a class="hover:text-blue-700" href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="hover:text-blue-700" href="#">Medicine</a>
+                    </li>
+                    <li>
+                        <a class="hover:text-blue-700" href="#">Order</a>            
+                    </li>
+                    <li>
+                        <a class="hover:text-blue-700" href="#">Profile</a>            
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    
+    <div class="text-center">
+        <h1 class="text-[#4200FF] text-4xl font-bold font-poppins mt-10">Manage Medicine</h1>
+    </div>
+    <div class="grid grid-cols-5 gap-4 mt-14 w-fit m-auto">
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="my-6 bg-white h-56 w-48 shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <img src="/medicine/delete.png" alt="/medicine/delete.png" class="ml-3 h-6 w-6 mt-3">
+                    <h1 class="font-poppins font-semibold text-black text-base text-center mt-3 ml-6">Cooling 5</h1>
+                </div>
+                <div>
+                    <img src="/medicine/cooling.png" alt="/medicine/cooling.png" class="bg-cover bg-center p-9 pt-3">
+                    <div class="bg-[hsl(256,100%,50%)] h-16 w-auto shadow-lg rounded-xl items-center -mt-6 flex">
+                        <div class="pl-5 pt-[0.10px]">
+                            <h1 class="font-poppins font-semibold text-white text-base text-left pt-2 -mt-1">Rp 25.000</h1>
+                            <h1 class="font-poppins font-semibold text-white text-xs text-left mt-2">Stok 70</h1>
+                        </div>
+                        <img src="/medicine/icon_edit.png" alt="/medicine/icon_edit.png" class="h-6 w-6 ml-14 mt-1">
+
+                    </div>
+                </div>
+            </div>
+
+    </div>
+    
+</body>
