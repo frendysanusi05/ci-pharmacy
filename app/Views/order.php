c2ee5dced8271df602b12f58b1d6c680835fe6e0
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2023-12-15 21:08:09 +0700

    ubah database

--- app/Views/order.php
+++ app/Views/order.php
@@ -93,7 +93,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -132,7 +132,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -171,7 +171,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -210,7 +210,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -249,7 +249,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -288,47 +288,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
-                        </div>
-                    </div>
-
-                </div>
-                <div>
-                    <h1 class="flex font-semibold text-black text-base text-center mt-3 m-auto ml-[72px]">#PS01 Vania Salsabila</h1>
-                    <table class="w-72 m-auto mt-3 ml-10">
-                        <tbody>
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Panadol</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Paracetamol</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Paratusin</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <div class="text-center">
-                        <button class="text-center m-auto mt-2 h-11 w-72 bg-[#9A9A9A] hover:bg-gray-500 text-white text-md font-medium font-poppins rounded-lg py-1 px-8">Done</button>
-                    </div>   
-                </div>
-            </div>
-        </div>
-
-
-        <div>
-            <div class="ml-4 my-6 bg-white h-56 w-[340px] shadow-lg rounded-xl aspect-square justify-center items-center">
-                <div class="flex">
-                    <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
-                        <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
-                        <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -368,46 +328,7 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
-                        </div>
-                    </div>
-
-                </div>
-                <div>
-                    <h1 class="flex font-semibold text-black text-base text-center mt-3 m-auto ml-[72px]">#PS01 Vania Salsabila</h1>
-                    <table class="w-72 m-auto mt-3 ml-10">
-                        <tbody>
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Panadol</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Paracetamol</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-
-                            <tr>
-                                <td class="font-semibold text-black text-sm">Paratusin</td>
-                                <td class="font-semibold text-black text-sm">1 pcs</td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <div class="text-center">
-                        <button class="text-center m-auto mt-2 h-11 w-72 bg-[#9A9A9A] hover:bg-gray-500 text-white text-md font-medium font-poppins rounded-lg py-1 px-8">Done</button>
-                    </div>   
-                </div>
-            </div>
-        </div>
-
-        <div>
-            <div class="ml-4 my-6 bg-white h-56 w-[340px] shadow-lg rounded-xl aspect-square justify-center items-center">
-                <div class="flex">
-                    <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
-                        <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
-                        <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
@@ -447,7 +368,86 @@
                     <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
                         <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
                         <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
-                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Unsuccessfull</h1>
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <h1 class="flex font-semibold text-black text-base text-center mt-3 m-auto ml-[72px]">#PS01 Vania Salsabila</h1>
+                    <table class="w-72 m-auto mt-3 ml-10">
+                        <tbody>
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Panadol</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Paracetamol</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Paratusin</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="text-center">
+                        <button class="text-center m-auto mt-2 h-11 w-72 bg-[#9A9A9A] hover:bg-gray-500 text-white text-md font-medium font-poppins rounded-lg py-1 px-8">Done</button>
+                    </div>   
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="ml-4 my-6 bg-white h-56 w-[340px] shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
+                        <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
+                        <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <h1 class="flex font-semibold text-black text-base text-center mt-3 m-auto ml-[72px]">#PS01 Vania Salsabila</h1>
+                    <table class="w-72 m-auto mt-3 ml-10">
+                        <tbody>
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Panadol</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Paracetamol</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+
+                            <tr>
+                                <td class="font-semibold text-black text-sm">Paratusin</td>
+                                <td class="font-semibold text-black text-sm">1 pcs</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="text-center">
+                        <button class="text-center m-auto mt-2 h-11 w-72 bg-[#9A9A9A] hover:bg-gray-500 text-white text-md font-medium font-poppins rounded-lg py-1 px-8">Done</button>
+                    </div>   
+                </div>
+            </div>
+        </div>
+
+
+        <div>
+            <div class="ml-4 my-6 bg-white h-56 w-[340px] shadow-lg rounded-xl aspect-square justify-center items-center">
+                <div class="flex">
+                    <div class="bg-[#FD7401] h-14 w-auto shadow-lg rounded-t-xl items-center -mt-6 mx-0 flex">
+                        <h1 class="font-poppins font-semibold text-white text-[11px] text-center ml-4">Order ID: #OBAT32145</h1>
+                        <div class="bg-[#CAFFD6] h-6 w-44 shadow-lg rounded-xl flex ml-6 mr-2">
+                            <h1 class="font-poppins font-semibold text-[#519120] text-[11px] text-center ml-4 py-1 px-2">Payment Successfull</h1>
                         </div>
                     </div>
 
